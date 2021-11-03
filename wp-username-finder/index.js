e44abb82da8ac9acaf1d2ee8d54aa7564dace18b
Ali Milani <amilani.ir@gmail.com>
2021-11-03 10:57:42 +0330

    Update wp-username-finder export all admins.

--- wp-username-finder/index.js
+++ wp-username-finder/index.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
 function logAdminUsername(url) {
+    // let adminList = []
     fetch(url)
         .then(res => res.json())
-        .then(out =>
-            console.log('Found! USERNAME: ', out[0]['slug']))
+        .then(out => console.log('Found! USERNAME/S: ', out.map(username => username['slug'])))
         .catch(err => { console.log('We can not find the admin :(') })
 
 }
 
-const domain = 'google-db.ir'
-logAdminUsername(`https://${domain}/wp-json/wp/v2/users/`)
+const domain = 'localhost:81/site2'
+logAdminUsername(`http://${domain}/wp-json/wp/v2/users/?per_page=100&page=1`)
